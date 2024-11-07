771f1b17098308de24d1fa3d3e5c79a67ad72d77
quankieu <quan31102002@gmail.com>
2024-11-07 23:03:07 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,21 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const hoidanit = "hehe";
+    // const hoidanit = 123;
+    // const hoidanit = true;
+    // const hoidanit = undefined;
+    // const hoidanit = [1, 2, 3];
+    const hoidanit = {
+        name: "hoidanIT",
+        age: 25
+    }
     return (
         <>
-            <div className=""> i and me hehe </div> 
+            <div className="">{JSON.stringify(hoidanit)} & i and me hehe </div>
+            <div>{console.log("ERIC")}</div>
             <div className="chile"
-            style={{borderRadius: "10px"}}
+                style={{ borderRadius: "10px" }}
             >chile</div>
         </>
 
